e94b6d6536a2d1605c048a4d9c6811c6da053347
nza <ivwi3103@gmail.com>
2025-12-11 17:03:24 +0700

    project file init

---
+++ sharify.py
@@ -0,0 +1,26 @@
+import tkinter as tk
+
+class SharifyApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Sharify")
+        self.root.geometry("720x480")
+
+    def read_file():
+        
+        
+    def write_file():
+    
+    def add_member():
+    
+    def view_members():
+        
+    def add_meal():
+        
+    def summary();
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = SharifyApp(root)
+    root.mainloop()
